51f810fea6ca599aaa798f418f0312746ec48d33
saikatnandisbr <saikat.nandi@superiorbusinessresults.com>
2024-03-20 06:39:40 +0700

    Added instructions to upload package to github repo

--- src/create_package.jl
+++ src/create_package.jl
@@ -1,10 +1,20 @@
+using PkgTemplates
+
 """
     function create_package(pkg_name::String)
 
-Create package using PkgTemplates
-"""
+Create package using PkgTemplates. 
+After creating package using this function follow the steps below to upload the package to github:
 
-using PkgTemplates
+    1. Create repo on github with name <pkg_anme>.jl
+
+    2. Upload local changes using following git commands as necessary
+
+        git remote -v                                            # shall include .git at end
+        git remote set-url origin <https URL with .git at end>   # if necessary run this command to update origin url
+        git remote -v                                            # shall include .git at end
+        git push -u origin main
+"""
 
 function create_package(pkg_name::String)
 
