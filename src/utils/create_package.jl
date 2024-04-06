2da8d1e164ee7081d7f6d367bd278b7b2efe3830
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-04-06 15:20:17 +0700

    Separate file for create_package

---
+++ src/utils/create_package.jl
@@ -0,0 +1,36 @@
+# imports
+using PkgTemplates
+
+# code
+"""
+    function create_package(pkg_name::String)
+
+Create package using PkgTemplates. 
+After creating package using this function follow the steps below to upload the package to github:
+
+    1. Create repo on github with name <pkg_anme>.jl
+
+    2. Upload local changes using following git commands as necessary
+
+        git remote -v                                            # url shall include .git at end
+        git remote set-url origin <https URL with .git at end>   # if necessary run this command to update origin url
+        git remote -v                                            # url shall include .git at end
+        git push -u origin main
+"""
+
+function create_package(pkg_name::String)
+
+    template = Template(;
+        user="saikatnandisbr",
+        authors=["Saikat Nandi"],
+        plugins=[
+            License(name="MIT"),
+            Git(),
+            !GitHubActions,   # may be later
+            !CompatHelper,    # may be later
+            !TagBot           # may be later
+        ],
+    )
+
+    template(pkg_name)
+end
