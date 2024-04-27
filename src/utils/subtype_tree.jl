3d6ea323affa6e635742dbb2580399417d551395
Saikat Nandi <saikat.nandi@superiorbusinessresults.com>
2024-04-27 20:17:25 +0700

    Use Let block to hide internals, other minor changes

--- src/utils/subtype_tree.jl
+++ src/utils/subtype_tree.jl
@@ -2,9 +2,13 @@
 using InteractiveUtils   # in base Julia
 
 # exports
-export show_subtype_tree
+export subtype_tree
 
 # code
+
+# let block to hide internals
+let
+    
 """
     function subtype_tree(root_type::Type)
 
@@ -13,18 +17,19 @@ Show subtype tree for given type.
 root_tye:   Type for which subtype tree is to be displayed
 """
 
-function subtype_tree(root_type::Type)
+global function subtype_tree(root_type::Type)
 
     # print root type
-    println(root_type)
+    formatted = string(root_type) * "$(isabstracttype(root_type) ? " (Abstract)" : " (Concrete)")"
+    println(formatted)
 
     # call function to show nested tree
     # use global constant for indentation
-    show_subtype_tree_nested(root_type, 1, g_indent_spaces)
+    subtype_tree_nested(root_type, 1, g_indent_spaces)
 end
 
 """
-    function show_subtype_tree_nested(root_type::Type, level::Int, indent::Int)
+    function subtype_tree_nested(root_type::Type, level::Int, indent::Int)
 
 Show subtype tree for given type.
 
@@ -33,13 +38,16 @@ level:      Current level in tree
 indent:     Number of spaces used to indent each level
 """
 
-function show_subtype_tree_nested(root_type::Type, level::Int, indent::Int)
+function subtype_tree_nested(root_type::Type, level::Int, indent::Int)
 
     for sub_type in subtypes(root_type)
         # print each substype with same indent
-        println(join(fill(" ", level * indent)) * string(sub_type))
+        formatted = join(fill(" ", level * indent)) * string(sub_type) * "$(isabstracttype(sub_type) ? " (Abstract)" : " (Concrete)")"
+        println(formatted)
 
         # recursively display subtypes of type with increased indent
-        show_subtype_tree_nested(sub_type, level+1, indent)
+        subtype_tree_nested(sub_type, level+1, indent)
     end
-end
+end    
+
+end # let
